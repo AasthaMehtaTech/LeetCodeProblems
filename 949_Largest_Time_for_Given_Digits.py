3755b4aba1dc88044d25d1873daccf92b5f918cf
AasthaGithub <aasthamehta2704@gmail.com>
2020-09-02 00:39:44 +0530

    Update 949_Largest_Time_for_Given_Digits.py
    Permutations, backtracking, DFS

--- 949_Largest_Time_for_Given_Digits.py
+++ 949_Largest_Time_for_Given_Digits.py
@@ -53,3 +53,106 @@ public:
     }
 };
 '''
+
+#fastest
+class Solution:
+    def largestTimeFromDigits(self, A: List[int]) -> str:
+        
+        str_A = [str(x) for x in A]
+        max_hour = [ str(x) for x in range(23,9,-1)]
+        max_zero = ["0"+ str(y) for y in range(9,-1,-1)]
+        hours = max_hour + max_zero 
+        max_minutes = ["5","4","3","2","1","0"]
+        
+        final_str = ""
+        for hour in hours:
+            cpy = [x for x in str_A] 
+            if hour[0] in cpy:
+                cpy.remove(hour[0])
+            
+                if hour[1] in cpy:
+                    final_str = hour[0]+hour[1]     
+                    cpy.remove(hour[1])
+                
+                    for min_ in max_minutes: 
+                        if min_ in cpy:
+                            cpy.remove(min_)
+                            final_str = final_str + ":" + min_ + cpy[0]
+                            return final_str                                
+        return ""
+    
+#zipping
+class Solution:
+    def largestTimeFromDigits(self, A: List[int]) -> str:
+        largest_time, max_mins = '', -1
+        
+        for p in itertools.permutations(A):
+            hours = p[0]*10 + p[1]
+            minutes = p[2]*10 + p[3]
+            
+            if hours < 24 and minutes < 60:
+                total_mins = hours*60 + minutes
+                
+                if total_mins > max_mins:
+                    max_mins = total_mins
+                    largest_time = '{}{}:{}{}'.format(*p)
+        return largest_time
+    
+#dfs
+class Solution:
+    def largestTimeFromDigits(self, A: List[int]) -> str:
+        times = []
+        def dfs(A = A, curr = []):
+            if len(A) == 0:
+                time = (10*curr[0]+curr[1], 10*curr[2]+curr[3])
+                if time[0] < 24 and time[1] < 60:
+                    times.append(time)
+                    
+#without permu library
+class Solution:
+    def largestTimeFromDigits(self, A: List[int]) -> str:
+        totalString = "".join(str(i) for i in A)
+        ways = [-1]
+        self.getPerm("", totalString, ways)
+        print(ways)
+        return "" if ways[-1] == -1 else str(ways[-1])[:2] + ":" + str(ways[-1])[2:]
+        
+    def getPerm(self, start, rest, ways):
+        if len(start) == 4:
+            if (self.isValid(start)):
+                ways.append(start)
+                if int(start) < int(ways[-2]):
+                    ways[-1], ways[-2] = ways[-2], ways[-1]
+        else:
+            for i in range(len(rest)):
+                other = rest[:i] + rest[i+1:]
+                self.getPerm(start+rest[i], other, ways)
+        
+        
+    def isValid(self, num):
+        a, b = num[:2], num[2:]
+        return 0 <= int(a) < 24 and 0 <= int(b) < 60
+                return
+            for i in range(len(A)):
+                temp = A[:i] + A[i+1:]
+                dfs(temp, curr + [A[i]])
+        dfs()
+        if len(times) == 0:
+            return ""
+        times.sort()
+        biggest = times[-1]
+        time = ""
+        if biggest[0] == 0:
+            time += "00"
+        elif biggest[0] < 10:
+            time += "0" + str(biggest[0])
+        else:
+            time += str(biggest[0])
+        time += ":"
+        if biggest[1] == 0:
+            time += "00"
+        elif biggest[1] < 10:
+            time += "0" + str(biggest[1])
+        else:
+            time += str(biggest[1])
+        return time
